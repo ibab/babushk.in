66ba5a521f2bc0091b997dc9cf3fd61c9cbfdbd8
Igor Babuschkin <igor@babushk.in>
2013-02-04 11:00:18 +0100

    Implemented site

--- about.md
+++ about.md
@@ -2,3 +2,8 @@
 title: About
 ---
 
+## About this website
+
+This website is served by a Linode running Apache.
+The posts have been written in [Markdown](http://en.wikipedia.org/wiki/Markdown) using [vim](http://www.vim.org/) and have been compiled to HTML with [Hakyll](http://jaspervdj.be/hakyll/), a static website generator written in [Haskell](http://www.haskell.org/haskellwiki/Haskell).
+
--- site.hs
+++ site.hs
@@ -1,74 +1,91 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Control.Applicative ((<$>))
-import Data.Monoid (mappend)
+import Data.Monoid ((<>), mappend, mconcat)
+import qualified Data.Map
 import Hakyll
 import Hakyll.Web.Pandoc
 import Text.Pandoc
 
-
 main = hakyll $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+  match "img/*" $ do
+    route   idRoute
+    compile copyFileCompiler
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+  match "css/*" $ do
+    route   idRoute
+    compile copyFileCompiler --compressCssCompiler
 
-    match (fromList ["about.md", "contact.md"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+  match "js/*" $ do
+    route   idRoute
+    compile copyFileCompiler
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompilerWith defaultHakyllReaderOptions pandocOptions
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+  match "about.md" $ do
+    route   $ setExtension "html"
+    compile $ myPandocC
+      >>= loadAndApplyTemplate "templates/default.html" postCtx
+      >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            let archiveCtx =
-                    field "posts" (\_ -> postList recentFirst) `mappend`
-                    constField "title" "Posts"                 `mappend`
-                    defaultContext
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $ myPandocC
+      >>= loadAndApplyTemplate "templates/post.html"  postCtx
+      >>= loadAndApplyTemplate "templates/default.html" postCtx
+      >>= relativizeUrls
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+  create ["archive.html"] $ do
+    route idRoute
+    compile $ do
+      let archiveCtx = field "posts" (\_ -> postList recentFirst)
 
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/archive.html"
+          archiveCtx
+        >>= loadAndApplyTemplate "templates/default.html"
+          (constField "title" "Archive" <> postCtx)
+        >>= relativizeUrls
 
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            let indexCtx = field "posts" $ \_ -> postList (take 3 . recentFirst)
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      let firstThree = \x -> do
+            posts <- recentFirst x
+            return $ take 3 posts
+      let indexCtx = field "posts" $ \_ -> postList firstThree
 
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" postCtx
-                >>= relativizeUrls
+      getResourceBody
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/default.html" postCtx
+        >>= relativizeUrls
 
-    match "templates/*" $ compile templateCompiler
-
+  match "templates/*" $ compile templateCompiler
 
 postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+postCtx = mconcat
+  [ mathjaxCtx
+  , dateField "date" "%B %e, %Y"
+  , defaultContext
+  ]
 
+myPandocC = pandocCompilerWith defaultHakyllReaderOptions pandocOptions
 
-postList :: ([Item String] -> [Item String]) -> Compiler String
+postList :: ([Item String] -> Compiler [Item String]) -> Compiler String
 postList sortFilter = do
-    posts   <- sortFilter <$> loadAll "posts/*"
-    itemTpl <- loadBody "templates/post-item.html"
-    list    <- applyTemplateList itemTpl postCtx posts
-    return list
+  posts    <- loadAll "posts/*"
+  filtered <- sortFilter posts
+  itemTpl  <- loadBody "templates/post-item.html"
+  list     <- applyTemplateList itemTpl postCtx filtered
+  return list
 
 pandocOptions = defaultHakyllWriterOptions
-  { writerHTMLMathMethod = MathML Nothing
+  { writerHTMLMathMethod = MathJax ""
   }
+
+mathjaxCtx = field "mathjax" $ \item -> do
+  metadata <- getMetadata (itemIdentifier item)
+  return $ case Data.Map.lookup "math" metadata of
+    Just "true" -> "<script type=\"text/x-mathjax-config\">MathJax.Hub.Config({messageStyle: \"none\",});</script>\
+                   \<script type=\"text/javascript\" src=\"http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML\" />"
+    Just _ -> ""
+    Nothing -> ""
+  
